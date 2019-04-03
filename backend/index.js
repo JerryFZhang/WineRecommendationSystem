13d1eff47e4918b6f7d925d0680813ecc64679b8
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2019-04-03 15:13:17 -0400

    load from file

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 var express = require('express')
 var app = express()
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('public/example.json');  
+let wines = JSON.parse(rawdata); 
 
 // Load server config for future deployment
 var serverConfig = require('./config.js').serverConfig
@@ -18,43 +22,14 @@ app.use(cors({
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/wines', (req, res) => {
-  //return all 
-  const json = {
-    "wines": [{
-      "id": "016840",
-      "name": "Bolla Valpolicella Classico",
-      "info": {
-        "price": "$14.95",
-        "size": "750 mL",
-        "description": "Bolla wines reflect true Italian style, and express the terroir of the Veneto region. Made from hand-harvested grapes, and aged for two months in oak. Enjoy aromas and flavours of cherry, raspberry and toasted nuts, with hints of licorice and spice. Serve with pizza, pasta or roast chicken with pomegranate glaze.",
-        "alcohol": "12.5%",
-        "location": "Veneto, Italy",
-        "sugar": "4 g/L"
-      }
-    }]
-  }
+
   //return the placeholder (for now)
-  res.send(json)
+  res.send(wines)
 })
 
 // `/rocket` endpoint that gets launch data from API
 app.get('/wine/:next', (req, res) => {
-  // if req.params.next exists return the wine only
-  // else return "Wine not found"
-  const json = {
-    "id": "016840",
-    "name": "Bolla Valpolicella Classico",
-    "info": {
-      "price": "$14.95",
-      "size": "750 mL",
-      "description": "Bolla wines reflect true Italian style, and express the terroir of the Veneto region. Made from hand-harvested grapes, and aged for two months in oak. Enjoy aromas and flavours of cherry, raspberry and toasted nuts, with hints of licorice and spice. Serve with pizza, pasta or roast chicken with pomegranate glaze.",
-      "alcohol": "12.5%",
-      "location": "Veneto, Italy",
-      "sugar": "4 g/L"
-    }
-  }
-  //return the placeholder (for now)
-  res.send(json)
+  res.send(wines)
 })
 
 
