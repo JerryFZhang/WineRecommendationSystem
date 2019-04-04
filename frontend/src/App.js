a689ad1118789a7e927ca788cf7d8e01c48c01b7
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2019-04-04 10:57:20 -0400

    formatted app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react'
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
+import {Route} from "react-router-dom";
 import { BrowserRouter as Router } from 'react-router-dom'
-// import Info from "./Info";
+import Info from "./Info";
 import List from "./List";
 class App extends Component {
   componentDidMount() {
@@ -17,9 +13,8 @@ class App extends Component {
       return (
       <div className='row pt-5'>
          <Router>
-          {/* <Route path="/" component={List}/> */}
           <Route path="/:page" component={List}/>
-          {/* <Route path="/info/:id" component={Info}/> */}
+          <Route path="/info/:id" component={Info}/>
           </Router>
     </div>
    );
